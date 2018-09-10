513d555f0ae9276c5742f9776348fb5f50da0925
Rachel <rnovick13@gmail.com>
2018-09-10 21:12:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function sayHiToGrandma(string) {
     return "I can't hear you.";
   } else if(shout) {
     return "YES INDEED!";
-  } else if ("I love you, Grandma") {
+  } else if ("I love you, Grandma.") {
     return "I love you, too.";
   }
 }
