13c66416721b63b62ca185b4882e29e7218a5615
Rachel <rnovick13@gmail.com>
2018-09-10 20:27:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ function logWhisper(string) {
   console.log(string.toLowerCase());
 }
 
-function sayHiToGrandma(string === .toLowerCase()) {
+function sayHiToGrandma() {
 console.log("I can't hear you!");
 }
