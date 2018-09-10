b500ea138949490376f9f2e5eb73672a7e10442c
Rachel <rnovick13@gmail.com>
2018-09-10 20:59:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ function logWhisper(string) {
 function sayHiToGrandma() {
   if (whisper) {
     return "I can't hear you";
-  } else if(shout) {
+  } else if (shout) {
     return "YES INDEED!";
-  } else if("I love you, Grandma") {
+  } else if ("I love you, Grandma") {
     return "I love you, too.";
   }
 }
